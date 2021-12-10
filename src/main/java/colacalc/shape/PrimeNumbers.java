232eeb1eb4748e1dfefcd6ea435d7ecbd304c240
Hussain Izhar <57052956+threepoint14159265359@users.noreply.github.com>
2021-12-10 17:09:49 +0100

    bug fix - PrimeNumber.java gives 4 as a prime

--- src/main/java/colacalc/shape/PrimeNumbers.java
+++ src/main/java/colacalc/shape/PrimeNumbers.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package colacalc.shape;
 import colacalc.exceptions.LengthOutOfRangeException;
-/**
- *
- * @author izhussai
- */
+
+
 public class PrimeNumbers {
     private long number; 
     
     public PrimeNumbers(long number) throws LengthOutOfRangeException{
-        if(number > 2147483647 || number < 1){
+        if(number > 2147483647 || number <= 1){
             throw new LengthOutOfRangeException("Input number is out of range.");
         }
         this.number = number; 
@@ -21,10 +15,10 @@ public class PrimeNumbers {
     
     public Boolean isPrime(){
         Boolean res = true; 
-        if (this.number < 1){
+        if (this.number <= 1){
             res = false; 
         }
-        for (int i = 2; i<this.number/2 ; i++){
+        for (int i = 2; i<=this.number/2 ; i++){
             if (this.number % i == 0){
                 res =  false; 
             }
